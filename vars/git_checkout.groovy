82967af5be8e0545be5e12fd23d9982bdcb8b72d
Avinash Kumar Sharma <134773362+avinash-sharma-121@users.noreply.github.com>
2025-05-21 10:09:39 -0700

    Create git_checkout.groovy

---
+++ vars/git_checkout.groovy
@@ -0,0 +1,5 @@
+def call(url,branch){
+      echo "code pulling"
+      git url:"https://github.com/avinash-sharma-121/react-testing-with-jenkins.git", branch: "main"
+      echo "code pull successfull"
+}
