2378e24448a3792f7309e6d444e610670106942d
Avinash <kumaravinashsharma82@gmail.com>
2025-12-16 13:52:01 +0530

    modified docker images creation lib

--- vars/docker_image_creation.groovy
+++ vars/docker_image_creation.groovy
@@ -1,12 +1,10 @@
-def call(image_name) {
+def call(String image_name, String tags, String docekr_username) {
     echo "code build with docker"
     sh '''
         whoami
-        which docker
         docker --version
-        docker ps
         pwd
         ls -l
     '''
-    sh 'docker build -t ${image_name} .'
+    sh 'docker build -t ' ${docekr_username}/${image_name}:${tags} .'
 }
